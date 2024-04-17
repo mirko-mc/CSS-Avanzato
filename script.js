1940420c002f9277abe18eed9e851b4bba5d86f8
DarkSide Labs <8547740+DarkSideLabs@users.noreply.github.com>
2024-04-17 21:18:22 +0200

    complete all CSS exercises and JS 1, 2

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
+document.querySelector("aside div:last-of-type li:has(+li:last-of-type)").remove();
 
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+function deletePadre(figlio) {
+    figlio.parentElement.remove();
+}
+const FONTWEIGHTBOLDS = document.querySelectorAll(".font-weight-bold");
+for (const FONTWEIGHTBOLD of FONTWEIGHTBOLDS) {
+    FONTWEIGHTBOLD.addEventListener("click", () => { deletePadre(FONTWEIGHTBOLD) })
+}
+const STRETCHEDLINKS = document.querySelectorAll(".stretched-link");
+for (const STRETCHEDLINK of STRETCHEDLINKS) {
+    STRETCHEDLINK.addEventListener("click", () => { deletePadre(STRETCHEDLINK) })
+}
 
-// EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
+// EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post
