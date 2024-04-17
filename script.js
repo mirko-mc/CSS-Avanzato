5ec17b5ec191d4c21fbf423f82ee6070388b881a
DarkSide Labs <8547740+DarkSideLabs@users.noreply.github.com>
2024-04-17 20:03:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
+
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+
+// EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
